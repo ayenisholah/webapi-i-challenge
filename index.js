c515925d9ec4112d6d3a6937bc7811d0c2489236
Shola Ayeni <Shola@Andelas-MacBook-Pro.local>
2019-07-08 13:42:31 +0100

    Create end point added

--- index.js
+++ index.js
@@ -5,17 +5,30 @@ const User = require('./data/db.js');
 
 server.use(express.json())
 
+// Read - Get the list o
 server.get('/api/users', (req, res) => {
     User.find()
         .then(data => {
             console.log('happy path');
             res.status(200).json(data);
         })
-        .catch(error => {
-            console.log('sad path');
-            res.json(error)
+        .catch(({ code, message }) => {
+            res.status(404).json({ err: 'Not Found' });
         });
 });
+
+// Create - Add a new user to the list
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+    console.log(req.body)
+    User.insert(newUser)
+    .then(addedUser => {
+        res.status(201).json(addedUser)
+    })
+    .catch(({ code, message }) => {
+        res.status(500).json({ err: 'There was an error while saving the user to the database' })
+    })
+})
 
 server.listen(3333, () => {
     console.log('listening on 3333')
